83ea8eb43c8aa2bc98c486b140c4a76c16c83472
imzain20 <imzainhafeez@gmail.com>
2024-10-18 23:56:53 +0500

    Removed additional comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
 
-// Register a new user
 router.post('/register', registerUser);
 
-// Log in a user
 router.post('/login', loginUser);
 
 module.exports = router;
--- backend/routes/pipelines.js
+++ backend/routes/pipelines.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const authenticateToken = require('../middleware/authenticateToken');
 const { getPipelines } = require('../controllers/pipelineController');
 
-// Fetch pipeline data (protected route)
 router.get('/', authenticateToken, getPipelines);
 
 module.exports = router;
